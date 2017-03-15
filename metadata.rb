52891e188040c1f84fae3553d76f4c0ad0f4bdca
Rikzg <rikzgut@gmail.com>
2017-03-15 12:11:12 +0100

    First upload

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'client_bootstrap'
+maintainer 'Frederik Gutzeit'
+license 'all_rights'
+description 'Bootstrap new Chef-Nodes'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.1.1'
+
---
+++ spec/unit/recipes/default_spec.rb
@@ -0,0 +1,20 @@
+#
+# Cookbook Name:: client_bootstrap
+# Spec:: default
+#
+# Copyright (c) 2017 The Authors, All Rights Reserved.
+
+require 'spec_helper'
+
+describe 'client_bootstrap::default' do
+  context 'When all attributes are default, on an unspecified platform' do
+    let(:chef_run) do
+      runner = ChefSpec::ServerRunner.new
+      runner.converge(described_recipe)
+    end
+
+    it 'converges successfully' do
+      expect { chef_run }.to_not raise_error
+    end
+  end
+end
